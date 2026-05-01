03c93dce889a913192bdb2ff02894acce2009ec5
Albert <albertaliev777@gmail.com>
2026-05-01 12:36:08 +0500

    initital commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed, ref, watch } from 'vue';
+
+const mode = ref('lte');
+const limit = ref(15);
+const value = ref(20);
+
+// const computedVal = computed({
+//   get() {
+//     return mode.value === 'lte' ? Math.min(value.value, limit.value) : Math.max(value.value, limit.value);
+//   },
+//   set(newVal) {
+//     value.value = newVal;
+//   }
+// })
+
+watch(limit, (newVal) => {
+  value.value = newVal;
+})
+</script>
+
+<template>
+  <div>
+    <h1>Hello Vue 3</h1>
+    <select v-model="mode">
+      <option value="lte">меньше чем или равно</option>
+      <option value="gte">больше чем или равно</option>
+    </select>
+    <input type="number" value="15" v-model="limit">
+    <br>
+    <hr>
+    <br>
+    <p>Целевое значение:
+      <input v-if="mode ==='lte'" type="number" v-model="value" :max="limit">
+      <input v-if="mode ==='gte'" type="number" v-model="value" :min="limit">
+    </p>
+  </div>
+</template>
