2688d0372e5ec93188fb91adbc1b0ff3538ab8a8
Albert <albertaliev777@gmail.com>
2026-05-06 12:50:27 +0500

    another way to make dynamic dashboard from JS Ninja

--- src/components/DashboardGitlab.vue
+++ src/components/DashboardGitlab.vue
@@ -8,25 +8,43 @@ const props = defineProps({
     }
 })
 
-// const gridAreas = computed(() => [`"${props.config[0].id} ${props.config[0].id} ${props.config[0].id}"`,
-//         `"${props.config[1].id} . ."`,
-//         `"${props.config[1].id} ${props.config[2].id} ${props.config[2].id}"`
-// ].join('\n'));
+// const panelStyles = computed(() => {
+//     return Object.fromEntries(props.config.map((entry) => [entry.id, {
+//         gridColumn: `${entry.gridAttributes.xPos + 1} / ${entry.gridAttributes.xPos + entry.gridAttributes.width + 1}`,
+//         gridRow: `${entry.gridAttributes.yPos + 1} / ${entry.gridAttributes.yPos + entry.gridAttributes.height + 1}`,
+//     }]))
+//     // + 1 добавляется, так как в CSS Grid линии нумеруются (индексация начинается) с 1, а у нас координаты задаются с нуля
+// })
 
-const panelStyles = computed(() => {
-    return Object.fromEntries(props.config.map((entry) => [entry.id, {
-        gridColumn: `${entry.gridAttributes.xPos + 1} / ${entry.gridAttributes.xPos + entry.gridAttributes.width + 1}`,
-        gridRow: `${entry.gridAttributes.yPos + 1} / ${entry.gridAttributes.yPos + entry.gridAttributes.height + 1}`,
-    }]))
-    // + 1 добавляется, так как в CSS Grid линии нумеруются (индексация начинается) с 1, а у нас координаты задаются с нуля
-})
+const columns = 12;
+const rows = computed(() =>
+  Math.max( ...props.config.map(({gridAttributes}) => gridAttributes.yPos + gridAttributes.height) )
+)
+
+const gridAreas = computed(() => {
+    const grid = Array.from({length: rows.value}, (_, i) => Array.from({length: columns}).fill('.'));
+
+    props.config.map((entry) => {
+        const { id, gridAttributes } = entry;
+        const { xPos, yPos, width, height } = gridAttributes;
+
+        for (let i = 0; i < width; i++) {
+            for (let j = 0; j < height; j++) {
+                grid[yPos + j][xPos + i] = id;
+            }
+        }
+    })
+
+    const result = grid.map((row) => `"${row.join(' ')}"`).join('\n');
+    return result;
+});
 
 </script>
 
 <template>
     <div class="grid">
-        <div v-for="entry in config" :key="entry.id" class="panel" :style="panelStyles[entry.id]">
-        <!-- <div v-for="entry in config" :key="entry.id" class="panel" :style="{ gridArea: entry.id }"> -->
+        <!-- <div v-for="entry in config" :key="entry.id" class="panel" :style="panelStyles[entry.id]"> -->
+        <div v-for="entry in config" :key="entry.id" class="panel" :style="{ gridArea: entry.id }">
             <slot :name="`panel(${entry.id})`">{{ entry.id }}</slot>
         </div>
     </div>
@@ -43,7 +61,7 @@ const panelStyles = computed(() => {
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     grid-template-rows: repeat(3, minmax(40px, auto));
-  /*  grid-template-areas: v-bind(gridAreas); */
+    grid-template-areas: v-bind(gridAreas);
 
     grid-gap: 8px;
     width: 100%;
